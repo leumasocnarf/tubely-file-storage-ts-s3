3ebc2c13b024135282b4692743c3aad0446e2f3e
samuel_franco <113880401+leumasocnarf@users.noreply.github.com>
2025-10-21 10:46:43 -0300

    feat: implement image encoding for db

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,13 +5,6 @@ import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
 const MAX_UPLOAD_SIZE = 10 << 20;
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
@@ -57,40 +50,12 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new Error("Error reading file data");
   }
 
-  videoThumbnails.set(videoId, {
-    data: fileData,
-    mediaType: file.type,
-  });
+  const encodedFile = Buffer.from(fileData).toString("base64");
+  const dataUrl = `data:${mediaType};base64,${encodedFile}`;
 
-  const thumbnailUrl = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
-
-  video.thumbnailURL = thumbnailUrl;
+  video.thumbnailURL = dataUrl;
 
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
-}
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
 }
