b82b5ef85f8addee74c11fce0667959f7e1bf43d
samuel_franco <113880401+leumasocnarf@users.noreply.github.com>
2025-10-21 01:24:59 -0300

    feat: implement thumbnail upload

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +11,65 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+
+const MAX_UPLOAD_SIZE = 10 << 20;
+
+export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("uploading thumbnail for video", videoId, "by user", userID);
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError(
+      "You don't have permission to upload thumbnail for this video"
+    );
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `File too large. Maximum size is ${MAX_UPLOAD_SIZE / (1024 * 1024)}MB`
+    );
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const fileData = await file.arrayBuffer();
+  if (!fileData) {
+    throw new Error("Error reading file data");
+  }
+
+  videoThumbnails.set(videoId, {
+    data: fileData,
+    mediaType: file.type,
+  });
+
+  const thumbnailUrl = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+
+  video.thumbnailURL = thumbnailUrl;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
+}
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -34,20 +93,4 @@ export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
       "Cache-Control": "no-store",
     },
   });
-}
-
-export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const token = getBearerToken(req.headers);
-  const userID = validateJWT(token, cfg.jwtSecret);
-
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
-
-  // TODO: implement the upload here
-
-  return respondWithJSON(200, null);
 }
